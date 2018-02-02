53b0789a39f3afd11d84e5759fc152708ba9ef73
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-02 15:34:14 +0000

    abstract callback from geolocation file

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-const request = require("request");
 const argv = require("./config");
 
-var encodedAddress = encodeURIComponent(argv.address);
+const geocode = require("./geocode/geocode.js");
 
-request(
-  {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-  },
-  (error, response, body) => {
-    if (error || body.status === "OVER_QUERY_LIMIT" || body.results === 0) {
-      console.log("Unable to connect to google servers... Try again!");
-    } else if (body.status === "ZERO_RESULTS") {
-      console.log("Unable to find that address.");
-    } else {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+geocode.geocodeAddress(argv.address,(error,result)=>{
+  if(error){
+    console.log(error)
+  }else{
+    console.log(JSON.stringify(result,undefined,2))
   }
-);
+});
+
---
+++ geocode/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+module.exports.geocodeAddress = (address, callback) => {
+  var encodedAddress = encodeURIComponent(address);
+  request(
+    {
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+      json: true
+    },
+    (error, response, body) => {
+      if (error || body.status === "OVER_QUERY_LIMIT" || body.results === 0) {
+        callback("Unable to connect to google servers... Try again!");
+      } else if (body.status === "ZERO_RESULTS") {
+        callback("Unable to find that address.");
+      } else {
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    }
+  );
+};
