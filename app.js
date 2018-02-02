b93896868552ff5176a9bc8d5073cb8e7d82b54c
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-02 15:07:48 +0000

    create argv config and encode user input

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const request = require('request');
+const request = require("request");
+const argv = require("./config");
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-  json: true
-}, (error, response, body) => {
-  console.log(JSON.stringify(body,undefined,2));
-});
+var encodedAddress = encodeURIComponent(argv.address);
+
+request(
+  {
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  },
+  (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
+);
---
+++ config.js
@@ -0,0 +1,16 @@
+const yargs = require("yargs");
+
+const argv = yargs
+    .option({
+        a:{
+            demand: true,
+            alias: "address",
+            describe:"Address to fetch weather",
+            string: true,
+        }
+    })
+    .help()
+    .alias("help","h")
+    .argv;
+
+module.exports = argv;
