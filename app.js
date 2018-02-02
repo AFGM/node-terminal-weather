05136685cab19591d53138922a562fbe3152f4ab
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-02 15:18:18 +0000

    handle request errors

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ var encodedAddress = encodeURIComponent(argv.address);
 
 request(
   {
-    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   },
   (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error || body.status === "OVER_QUERY_LIMIT" || body.results === 0) {
+      console.log("Unable to connect to google servers... Try again!");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("Unable to find that address.");
+    } else {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
   }
 );
